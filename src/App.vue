bae936de555e2351763013d39478c4b4b77abfb8
Sathibabu <sathibabu519@gmail.com>
2020-11-16 20:17:22 +0530

    Compoenent rename changes

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,16 @@
 <template>
   <div id="app">
-    <HelloWorld />
+    <Comments />
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import Comments from "./components/Comments.vue";
 
 export default {
   name: "App",
   components: {
-    HelloWorld,
+    Comments,
   },
 };
 </script>
